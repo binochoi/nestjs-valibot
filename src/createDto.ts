15cad79c506402eb10807275d1b1b2ba298227cf
bino <bonoself@gmail.com>
2024-10-14 14:32:59 +0900

    refactor:

--- src/createDto.ts
+++ src/createDto.ts
@@ -1,17 +1,17 @@
-import { parse, BaseSchema } from 'valibot';
+import * as v from 'valibot';
 
 export interface ValibotDto<TOutput = any> {
     new (): TOutput
     isValibotDto: true
-    schema: BaseSchema<TOutput>
+    schema: v.BaseSchema<any, TOutput, any>
     create(input: unknown): TOutput
 }
 
 export const createDto = <
-    TInput = object
->(schema: BaseSchema<TInput>) =>
+    TInput = object,
+>(schema: v.BaseSchema<TInput, any, any>) =>
     class DtoGenerator {
         public static isValibotDto = true;
         public static schema = schema;
-        public static create = (input: unknown) => parse(schema, input);
+        public static create = (input: unknown) => v.parse(schema, input);
     } as ValibotDto<TInput>
